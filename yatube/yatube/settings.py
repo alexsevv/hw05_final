6e92a75cfa3b697c1046700fd28ae45edb6e3257
Александр Матияка <matiacka@yandex.ru>
2022-12-01 12:27:09 +0300

    add backspace for pep8

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'qcfiw+ydu5un%cx2lv9qy)h&xk(b0gb9(2n3c83i1np(s+63u8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-#DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
